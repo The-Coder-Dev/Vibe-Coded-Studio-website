6cc3825b340a92230704b323cb45059725270cc9
Dev Sharma <devsharma2057@gmail.com>
2026-05-24 17:27:50 +0530

    feat: integrate Vercel Speed Insights and initialize root layout with standard metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import ConditionalLayout from "@/components/ConditionalLayout";
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           {children}
         </ConditionalLayout>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
